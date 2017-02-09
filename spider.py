f53ed1019ad4ac2921062677a8d67eb26b13c612
dengshilong <dengshilong1988@gmail.com>
2017-02-09 14:36:24 +0800

    fix: add logger and doctest

--- spider.py
+++ spider.py
@@ -1,19 +1,38 @@
 import click
 import urllib.request
 from bs4 import BeautifulSoup
-
+import logging
 
 old_urls = set()
+LOGGER = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(asctime)s %(name)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
 
 
 def get_repsonse(url):
+    """
+    Simple dict but also support access as x.y style.
+
+    >>> response = get_repsonse('')
+    >>> response
+    """
     try:
         response = urllib.request.urlopen(url).read()
         return response
     except Exception as e:
-        print(e)
+        logging.warning('get response error {}'.format(e))
+        return None
 
 def get_all_urls(response):
+    """
+    >>> response = 'sfsd<a href="http://www.bai.com">sdfs</a>'
+    >>> urls = get_all_urls(response)
+    >>> urls
+    ['http://www.bai.com']
+    """
     soup = BeautifulSoup(response, 'html.parser')
     urls = []
     for link in soup.find_all('a'):
@@ -25,28 +44,56 @@ def save_url(url, response):
     pass
 
 
-def _spider(url, deep):
+def spider(url, deep):
     if url in old_urls or deep == 0:
         return
     print(url)
+    LOGGER.info('crawling {}'.format(url))
     response = get_repsonse(url)
     if not response:
         return
     save_url(url, response)
     old_urls.add(url)
     urls = get_all_urls(response)
-    print(urls)
     for u in urls:
-        _spider(u, deep - 1)
+        spider(u, deep - 1)
+
+def process_log(file, level):
+    handler = logging.FileHandler(filename=file)
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+    LOGGER.addHandler(handler)
+    if level <= 1:
+        LOGGER.setLevel(logging.DEBUG)
+    elif level == 2:
+        LOGGER.setLevel(logging.INFO)
+    elif level == 3:
+        LOGGER.setLevel(logging.WARNING)
+    elif level == 4:
+        LOGGER.setLevel(logging.ERROR)
+    else:
+        LOGGER.setLevel(logging.CRITICAL)
+    return LOGGER
 
 
 @click.command()
 @click.option('--url', '-u', help='seed url')
 @click.option('--deep', '-d', default=1,
               help='crawl deep')
-def spider(url, deep):
-    _spider(url, deep)
+@click.option('--file', '-f', default='spider.log',
+              help='log file')
+@click.option('--level', '-l', default=1,
+              help='log level')
+@click.option('--testself', is_flag=True)
+def main(url, deep, file, level, testself):
+    if testself:
+        import doctest
+        doctest.testmod()
+        return
+    process_log(file, level)
+    spider(url, deep)
 
 
 if __name__ == "__main__":
-    spider()
+    main()
